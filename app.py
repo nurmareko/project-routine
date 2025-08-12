d18eef134eb9e17a443da2cfebdd902c3b996eeb
nurmareko <nurmareko@yandex.ru>
2025-08-12 22:55:27 +0700

    Add response caching control and update form action in index page

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def index():
@@ -11,3 +18,7 @@ def index():
 @app.route("/edit")
 def edit():
     return render_template("edit.html")
+
+@app.route("/study", methods=["POST"])
+def study():
+    return render_template("study.html")
