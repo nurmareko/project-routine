56a90f3203807248b3fb6ed0e5d09660a141abb6
nurmareko <nurmareko@yandex.ru>
2025-08-12 22:39:28 +0700

    Add edit page and update index layout for navigation

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/edit")
+def edit():
+    return render_template("edit.html")
