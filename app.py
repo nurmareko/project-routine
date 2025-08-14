f3b38cd08e1280670617e08068e1d38d8fc00c1d
nurmareko <nurmareko@yandex.ru>
2025-08-14 14:43:58 +0700

    Implement the reading of data from database to show the task options

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from cs50 import SQL
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+db = SQL("sqlite:///routine.db")
 
 @app.after_request
 def after_request(response):
@@ -11,20 +14,27 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    tasks = db.execute("SELECT * FROM tasks")
+
     if request.method == "POST":
-        return render_template("index.html")
+        return render_template("index.html", tasks=tasks)
+
     else:
-        return render_template("index.html")
+        return render_template("index.html", tasks=tasks)
+
 
 @app.route("/edit")
 def edit():
     return render_template("edit.html")
 
+
 @app.route("/study", methods=["POST"])
 def study():
     return render_template("study.html")
+
 
 @app.route("/statistics")
 def statistics():
