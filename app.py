b286cc02719b800e9876a9303fb912905151a9ff
nurmareko <nurmareko@yandex.ru>
2025-08-16 16:52:08 +0700

    Add session management and flash messages; update delete functionality

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
+# Configure application
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///routine.db")
 
 @app.after_request
@@ -43,7 +51,13 @@ def statistics():
     return render_template("statistics.html", studies=studies)
 
 
-@app.route("/delete", methods=["POST"])
+@app.route("/delete", methods=["GET", "POST"])
 def delete():
-    name = request.form.get("name")
-    return render_template("delete.html", name=name)
+    if request.method == "POST":
+        name = request.form.get("name")
+        db.execute("DELETE FROM tasks WHERE name=?", name)
+        flash(name + " deleted!")
+        return redirect("/tasks")
+    else:
+        name = request.args.get("name")
+        return render_template("delete.html", name=name)
